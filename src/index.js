2217b37c46d5cacb8f24dfbe1ad4c3efb970f185
Jesus Bueno <34608190+jejobueno@users.noreply.github.com>
2022-01-17 13:46:12 +0100

    changing index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Router from 'router';
 import queryString from 'query-string';
 import Q from 'q';
 import { env } from './helper';
+import fetch from 'node-fetch';
 
 const app = Router({
   mergeParams: true,
@@ -16,27 +17,9 @@ mimikModule.exports = (context, req, res) => {
 
 // Sample HTTP Request
 app.get('/', (req, res) => {
-  const PiCamera = require('pi-camera');
-  const myCamera = new PiCamera({
-    mode: 'photo',
-    width: 640,
-    height: 480,
-    nopreview: true,
-  });
-
-  myCamera.snapDataUrl()
-    .then((result) => {
-    // Your picture was captured
-    res.end('<img src="${result}">');
-    })
-    .catch((error) => {
-     // Handle your error
-    });
-});
-
-// Sample HTTP Request to return the environment variables
-app.get('/env', (req, res) => {
-  res.end(JSON.stringify(env()));
+  const response = await fetch('https://api.github.com/users/github');
+  const data = await response.json();
+  res.end(JSON.stringify(data))
 });
 
 // Sample HTTP Request with Parameters
@@ -44,21 +27,4 @@ app.get('/sayHello/:name', (req, res) => {
   res.end(`Hello ${req.params.name}`);
 });
 
-// Sample HTTP Request with Query
-app.get('/add', (req, res) => {
-  const query = queryString.parse(req._parsedUrl.query);
-  res.end(`result is ${Number(query.x) + Number(query.y)}`);
-});
 
-// Sample Promise with Q
-app.get('/promise', (req, res) => {
-  const simplePromise = Q.Promise((resolve) => {
-    resolve('From the Q promise.');
-  });
-  simplePromise.then(answer => res.end(answer));
-});
-
-// Sample HTTP request with JSON Body and return it
-app.post('/form', (req, res) => {
-  res.end(req.body);
-});
